8d172fb506c4ae7a8d5cbe280ef3618c70ac0ce0
Gabin221 <serrurot19@gmail.com>
2024-04-08 09:24:19 +0200

    Update launchercode.sh

--- launchercode.sh
+++ launchercode.sh
@@ -23,6 +23,10 @@ do
        touch $nom_fichier 
        echo -e "IDENTIFICATION DIVISION.\nPROGRAM-ID. Hello-world.\nPROCEDURE DIVISION.\n\tDISPLAY \"Hello World!\".    ." > $nom_fichier
        ;;
+       "coffee") 
+       touch $nom_fichier 
+       echo -e "console.log \"Hello World!\"" > $nom_fichier
+       ;;
        "cpp") 
        touch $nom_fichier 
        echo -e "#include <iostream>\n\nusing namespace std;\n\nint main() {\n\tcout << \"Hello World!\" << endl;\n\treturn 0;\n}" > $nom_fichier
@@ -35,9 +39,21 @@ do
        touch $nom_fichier 
        echo -e "using System;\nusing System.Collections.Generic;\nusing System.Linq;\nusing System.Text;\nusing System.Threading.Tasks;\n\nnamespace ConsoleApp1\n{\n\tclass Program\n\t{\n\tstatic void Main(string[] args)\n\t{\n\t\tConsole.WriteLine("Hello, world!");\n\t\tConsole.ReadLine();\n\t}\n\t}\n}" > $nom_fichier
        ;;
+       "dart") 
+       touch $nom_fichier 
+       echo -e "main(){\n\tprint(\"Hello World!\");\n}" > $nom_fichier
+       ;;
+       "dpr") 
+       touch $nom_fichier 
+       echo -e "program HelloWorld;\nbegin\n\tWriteLn(\"Hello World!\");\nend." > $nom_fichier
+       ;;
        "go") 
        touch $nom_fichier 
        echo -e "package main\n\nimport "fmt";\n\nfunc main() {\n\tfmt.Println(\"Hello World!\")\n}" > $nom_fichier
+       ;;
+       "hs") 
+       touch $nom_fichier 
+       echo -e "module Main where\n\nmain :: IO ()\nmain = putStrLn \"Hello World!\"" > $nom_fichier
        ;;
        "html") 
        touch $nom_fichier 
@@ -55,9 +71,17 @@ do
        touch $nom_fichier 
        echo -e "fun main(args : Array<String>) {\n\tprintln(\"Hello, World!\")\n}" > $nom_fichier
        ;;
+       "lhs") 
+       touch $nom_fichier 
+       echo -e "module Main where\n\nmain :: IO ()\nmain = putStrLn \"Hello World!\"" > $nom_fichier
+       ;;
        "m") 
        touch $nom_fichier 
        echo -e "disp(\"Hello World!\");" > $nom_fichier
+       ;;
+       "pas") 
+       touch $nom_fichier 
+       echo -e "program Hello;\nbegin\n\twriteln (\"Hello World!\");\nend." > $nom_fichier
        ;;
        "php") 
        touch $nom_fichier 
